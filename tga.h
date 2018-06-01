d765024c3f3cbad28241cb41383e5870cbf42464
Lukas Genkinger <lukas.genkinger@web.de>
2018-06-01 11:32:49 +0200

    fix

--- tga.h
+++ tga.h
@@ -1,11 +1,20 @@
 #ifndef TGA_LOADER_H
 #define TGA_LOADER_H
 
-#include "sandboxdef.h"
+#include <stdint.h>
 
 
 #define TGA_RGBA 0x00
 #define TGA_BGRA 0x01
+
+typedef uint8_t u8;
+typedef uint16_t u16;
+typedef uint32_t u32;
+typedef int8_t i8;
+typedef int16_t i16;
+typedef int32_t i32;
+
+
 
 struct tga_image_header
 {
